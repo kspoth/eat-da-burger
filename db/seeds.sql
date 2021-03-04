997a4dda9f8ae78fc3d187bc73a07ac935adc46a
Kevin Spoth <kspoth08@su.edu>
2021-03-03 20:27:25 -0500

    updated js

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
-INSERT INTO burgers (burger_name) VALUES ("Bleu Cheese Burger");
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
