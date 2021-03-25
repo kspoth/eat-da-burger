a253543c71a1d74b89095747d532bcce8f9a844a
Kevin Spoth <kspoth08@su.edu>
2021-03-24 22:22:50 -0400

    change_mysql

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,25 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Martha#1sys_config",
-    database: "burgers_db",
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Martha#1sys_config",
+//     database: "burgers_db",
+//   });
+// }
+
+connection = mysql.createConnection({
+  host: "u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "k2nhrfbi8i7ksey3",
+  password: "zi1s6qb7bd7f5pz1",
+  database: "zh97fp0uqk6dpk7p",
+});
 
 connection.connect(function (err) {
   if (err) {
